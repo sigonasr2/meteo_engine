ff520f5814a64cd2cab137ae7343552d02dda681
Nic0Nic0Nii <sigonasr2@yahoo.com>
2021-09-30 04:30:34 +0000

    Add setup shell file and coauthors file.

---
+++ coauthors.sh
@@ -0,0 +1,12 @@
+npm i -g git-mob
+cat <<-EOF > ~/.git-coauthors
+{
+  "coauthors": {
+    "sig": {
+      "name": "sigonasr2",
+      "email": "sigonasr2@gmail.com"
+    }
+  }
+}
+EOF
+git mob sig
